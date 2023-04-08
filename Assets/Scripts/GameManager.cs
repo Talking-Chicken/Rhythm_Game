fb3385bb21ccc1c06adf6e9076726ef4d351a568
Leo Ji <sj3026@nyu.edu>
2023-04-07 22:04:14 -0400

    sound bank update

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,9 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : Singleton<GameManager>
 {
     public int Score;
     public bool IsLeftTurn = true;
+
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.R))
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
 }
